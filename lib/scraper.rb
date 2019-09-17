b8626511d719f66b5f380e7c604b852a5dc005ef
chic-page-6502 <zacharytq@gmail.com>
2019-09-17 23:22:18 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,13 +6,15 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     html = Nokogiri::HTML(open(index_url))
-
-    html.css("div.student-card").map do |student|
+    students = []
+    html.css("div.student-card").each do |student|
       student_hash = {}
-      student_hash[:name] = html.css("h4.student-name").text
-      student_hash[:location] = html.css("p.student-location").text
-      student_hash
+      student_hash[:name] = student.css("h4.student-name").text
+      student_hash[:location] = student.css("p.student-location").text
+      student_hash[:profile_url] = student.css("a").attribute("href").value
+      students << student_hash
     end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
