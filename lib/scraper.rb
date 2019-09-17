936481e176e51d8c9ba0704bf7ae7811cc58eea8
chic-page-6502 <zacharytq@gmail.com>
2019-09-17 23:16:40 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,22 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = Nokogiri::HTML(open(index_url))
+
+    html.css("div.student-card").map do |student|
+      student_hash = {}
+      student_hash[:name] = html.css("h4.student-name").text
+      student_hash[:location] = html.css("p.student-location").text
+      student_hash
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
