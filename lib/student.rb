eab3a74e0d2ee612e1502b3c0f4f2417f9b4b745
chic-page-6502 <zacharytq@gmail.com>
2019-09-18 23:32:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each {|student| Student.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
